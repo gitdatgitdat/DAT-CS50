8659e626856e710730775383e13cc775d91c8c95
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-02 23:03:08 -0800

    Update mario.c
    Tinkering with week 1 problem. Getting the user input in the range of 1 to 8. Could we also do the advanced problem that includes spacing?

--- Week 1/Code/mario.c
+++ Week 1/Code/mario.c
@@ -1,14 +1,27 @@
+#include <cs50.h>
 #include <stdio.h>
 
+int get_positive_int(void)
 void print_row(int n);
 
 int main(void)
 {
-    const int n = 3;
-    for (int row = 0; row < n row++)
+    const int n = get_positive_int();
+    for (int row = 0; row < n; row++)
     {
         print_row(n);
     }
+}
+
+int get_positive_int(void)
+{
+        int n;
+    do
+    {
+         n = get_int("Number: ");
+    }
+    while (n < 1);
+    return n;
 }
 
 void print_row(int n)
