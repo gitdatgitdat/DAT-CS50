d2a3e07d4166a7fb83c6307d3efaf8afa5435835
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-07 15:10:27 -0800

    Update cash.c
    This makes sense to me but I imagine there must be a simpler way. There's a lot of repetition in the code but I'm not sure how to articulate that effectively.

--- Week 1/Code/cash.c
+++ Week 1/Code/cash.c
@@ -1,42 +1,98 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int get_positive_int(void);
+int get_cents(void);
+int get_quarters(int cents);
+int get_dimes(int cents);
+int get_nickels(int cents);
+int get_pennies(int cents);
 
 int main(void)
 {
-    const int n = get_positive_int();
-    int change = 0;
+    // Asking user for the changed owed and setting owed to zero
+    int cents = get_cents();
+    int owed = 0;
 
-    // Prompt the user for change owed, in cents
+    // Calculate the number of quarters then subtracting the value from cents
+    int quarters = get_quarters(cents);
+    cents -= quarters * 25;
+    owed += quarters;
 
-    // Calculate how many quarters you should give customer
-    // Subtract the value of those quarters from cents
+    // Calculate the number of dimes then subtracting the value from cents
+    int dimes = get_dimes(cents);
+    cents -= dimes * 10;
+    owed += dimes;
 
-    // Calculate how many dimes you should give customer
-    // Subtract the value of those dimes from remaining cents
+    // Calculate the number of nickels then subtracting the value from cents
+    int nickels = get_nickels(cents);
+    cents -= nickels * 5;
+    owed += nickels;
 
-    // Calculate how many nickels you should give customer
-    // Subtract the value of those nickels from remaining cents
+    // Calculate the number of pennies then subtracting the value from cents
+    int pennies = get_pennies(cents);
+    cents -= pennies * 1;
+    owed += pennies;
 
-    // Calculate how many pennies you should give customer
-    // Subtract the value of those pennies from remaining cents
-
-    // Sum the number of quarters, dimes, nickels, and pennies used
-    // Print that sum
-
+    // Print the sum of owed
+    printf("%i\n", owed);
 }
 
-// Function for getting number from user
-int get_positive_int(void)
+// Have user provide integer to be used for cents
+int get_cents(void)
 {
-    int n;
+    int cents;
     do
     {
-        n = get_int("Change owed: ");
+        cents = get_int("Change owed: ");
     }
-    while (n < 0);
-    return n;
+    while (cents < 0);
+    return cents;
 }
 
+// Determine number of quarters
+int get_quarters(int cents)
+{
+    int quarters = 0;
+    while (cents >= 25)
+    {
+        quarters++;
+        cents = cents - 25;
+    }
+    return quarters;
+}
 
+// Determine number of dimes
+int get_dimes(int cents)
+{
+    int dimes = 0;
+    while (cents >= 10)
+    {
+        dimes++;
+        cents = cents - 10;
+    }
+    return dimes;
+}
+
+// Determine number of nickels
+int get_nickels(int cents)
+{
+    int nickels = 0;
+    while (cents >= 5)
+    {
+        nickels++;
+        cents = cents - 5;
+    }
+    return nickels;
+}
+
+// Determine number of pennies
+int get_pennies(int cents)
+{
+    int pennies = 0;
+    while (cents >= 1)
+    {
+        pennies++;
+        cents = cents - 1;
+    }
+    return pennies;
+}
