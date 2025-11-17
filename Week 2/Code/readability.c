3985d80e81450dd728c7dddf755f5261c80d51f6
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-16 22:48:01 -0800

    Update readability.c

--- Week 2/Code/readability.c
+++ Week 2/Code/readability.c
@@ -7,14 +7,16 @@
 int main(void)
 {
     // Prompt user for text
-        // If does not contain one word, starts or ends with a space, and does not have multiple spaces in a row
+        // If does not contain one word, 
+        // starts or ends with a space, 
+        // and does not have multiple spaces in a row
 
     // Count the number of letters, words, and sentences in the text
 
     // Compute the Coleman-Liau index
-    // index = 0.0588 * L - 0.296 * S - 15.8
-    // L = average number of letters per 100 words
-    // S = average number of sentences per 100 words
+        // index = 0.0588 * L - 0.296 * S - 15.8
+        // L = average number of letters per 100 words
+        // S = average number of sentences per 100 words
 
     //Print the grade level
 }
