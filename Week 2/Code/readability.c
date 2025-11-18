7204339ee3c862d03741f73009bb70e11e371fd1
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-18 11:28:01 -0800

    Update readability.c
    Created sentence counting function. Added printf test to make sure it works and successful so far. Should add checks to the input function in main to only take correct text input.

--- Week 2/Code/readability.c
+++ Week 2/Code/readability.c
@@ -27,11 +27,17 @@ int main(void)
         // S = average number of sentences per 100 words
 
     //Print the grade level
+    printf("Letter count: %i\n", letters);
+    printf("Word count: %i\n", words);
+    printf("Sentence count: %i\n", sentences);
 }
 
 int count_letters(string text)
 {
+    // Set variable for number of letters
     int letter_count = 0;
+    // Go through the string until you hit nul
+    // Increase letter_count by one for each alpha character
     for (int i = 0; text[i] != '\0'; i++)
     {
         if (isalpha(text[i]))
@@ -39,24 +45,44 @@ int count_letters(string text)
             letter_count++;
         }
     }
+    // Return sum to main
+    
+    // Test to determine if functions are producing correct sums
     return letter_count;
 }
 
 int count_words(string text)
 {
+    // Set variable for number of words
     int word_count = 1;
+    // Determine length of string
     int len = strlen(text);
+    // Run for loop based on length of string
     for(int i = 0; i < len; i++)
     {
+        // Go through the string array, but don't count spaces (32 in ASCII)
         if(text[i] == 32 && i != 0)
         {
             word_count++;
         }
     }
+    // Return sum to main
     return word_count;
 }
 
 int count_sentences(string text)
 {
-    // Return the number of sentences in text
+    // Set variable for number of sentences
+    int sentences = 0;
+    // Go through string until you hit nul
+    for (int i = 0; text[i] != '\0'; i++)
+    {
+        // When detecting a '.' '?' or '!' increment variable by 1
+        if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+    // Return sum to main
+    return sentences;
 }
