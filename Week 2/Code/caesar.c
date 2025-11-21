541c1a6bb452ad58be406eb83fd105df873336e7
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-20 21:31:04 -0800

    Create caesar.c
    Laid down psuedo code.

---
+++ Week 2/Code/caesar.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// I need to remove void and add input.
+// The user provides an input less than or equal to 26
+int main(int agrc, string argv[])
+{
+
+    // Take text input
+    // string get_string(plaintext:  );
+
+    // Rotate the string values by the key value entered with the program
+    // Ceaar's formula:
+    // c base of i = (p base of i + k) % 26
+    // c = cipher and the result we're after
+    // p = plaintext that is input by the user
+    // k = key, which is provided on startup
+    // % refers to the remainder when dividing by, in this case, 26
+
+    // Print the string back to the user
+    // printf("ciphertext %s", c)
+}
