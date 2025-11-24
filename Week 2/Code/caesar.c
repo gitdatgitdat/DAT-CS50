8c2f893a5e875ae4fc7f8e0aa99e4c182faafe90
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-11-23 22:25:51 -0800

    Caesar complete, first short done
    Caesar problem completed. Need to finish shorts.

--- Week 2/Code/caesar.c
+++ Week 2/Code/caesar.c
@@ -1,7 +1,8 @@
-#include <ctype.h>
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // The user provides an input less than or equal to 26
 int main(int argc, string argv[])
@@ -29,25 +30,61 @@ int main(int argc, string argv[])
     // Take text input from user
     string ptext = get_string("plaintext:  ");
 
-    // For each char in plaintext;
-        // Rotate by the key value entered with the program
-            // Preserve upper and lowercase
-            // isalpha
-            // islower
-            // isupper
-        // If not alpha, make no changes
+    // Begin output
+    printf("ciphertext: ");
 
-    // Ceaar's formula:
-    // c base of i = (p base of i + k) % 26
-    // c = cipher and the result we're after
-    // p = plaintext that is input by the user
-    // k = key, which is provided on startup
-    // % refers to the remainder when dividing by, in this case, 26
-        // To be able to use this
-        // ASCII > Alphabetical (0 to 26)
-        // Shift using Caesar's formula
-        // Alphabetical > ASCII
+    // For loop through ptext
+    for (int i = 0; i < strlen(ptext); i++)
+    {
+        // Current element of ptext
+        char c = ptext[i];
 
-    // Print the string back to the user
-    // printf("ciphertext %s", c)
+        // If alpha, run through this loop
+        if (isalpha(c))
+        {
+            // If upper, rotate here
+            if (isupper(c))
+            {
+                int pi = c - 'A';
+                int ci = (pi + key) % 26;
+                char encrypt = ci + 'A';
+                printf("%c", encrypt);
+            }
+            // If alpha but not upper, must be lower
+            // Rotate here
+            else
+            {
+                int pi = c - 'a';
+                int ci = (pi + key) % 26;
+                char encrypt = ci + 'a';
+                printf("%c", encrypt);
+            }
+        }
+        // If not alpha, don't rotate
+        else
+        {
+            printf("%c", c);
+        }
+    }
+    printf("\n");
 }
+
+// For each char in plaintext;
+// Rotate by the key value entered with the program
+// Preserve upper and lowercase
+// isalpha
+// islower
+// isupper
+// If not alpha, make no changes
+
+// Ceaar's formula:
+// c base of i = (p base of i + k) % 26
+// c = cipher and the result we're after
+// p = plaintext that is input by the user
+// k = key, which is provided on startup
+// % refers to the remainder when dividing by, in this case, 26
+// To be able to use this
+// ASCII > Alphabetical (0 to 26)
+// Shift using Caesar's formula
+// Alphabetical > ASCII
+
---
+++ Week 3/Shorts/Linear Search.md
@@ -0,0 +1,11 @@
+Linear search the idea is to iterate across the array form left to right searching for a specified element.
+
+Repeat, starting at the first element
+	If the first element is the atrget, stop
+	Else, move to next element
+	
+If what we're searching for isn't in the array, we can still run a linear search. After running through the array, the search will say it failed to find that element.
+
+Worst case, this will have to look through every element.
+
+Best case, the target is the first element.
