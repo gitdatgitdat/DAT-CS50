7445735e2fee9820f65e826acbe2aeb099aa75d3
gitdatgitdat <141184949+gitdatgitdat@users.noreply.github.com>
2025-12-20 23:58:37 -0800

    Update helpers.c
    Working through filter-less problem

--- Week 4/Code/filter-less/helpers.c
+++ Week 4/Code/filter-less/helpers.c
@@ -1,15 +1,53 @@
+#include <math.h>
 #include "helpers.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int red = image[i][j].rgbtRed;
+            int green = image[i][j].rgbtGreen;
+            int blue = image[i][j].rgbtBlue;
+
+            int avg = (int) round((red + green + blue) / 3.0);
+
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtGreen = avg;
+            image[i][j].rgbtBlue = avg;
+        }
+    }
 }
 
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    int cap(int value)
+    {
+        if (value > 255)
+            return 255;
+        return value;
+    }
+
+    for (int i = 0; i < height; i++)
+    {
+        for int j = 0; j < width; j++)
+        {
+            int originalRed = image[i][j].rgbtRed;
+            int originalGreen = image[i][j].rgbtGreen;
+            int originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round(0.393 * originalRed + 0.769 * originalGreen + 0.189 * originalBlue);
+            int sepiaGreen = round(0.349 * originalRed + 0.689 * originalGreen + 0.168 * originalBlue);
+            int sepiaBlue = round(0.272 * originalRed + 0.534 * originalGreen + 0.131 * originalBlue);
+
+            sepiaRed = cap(sepiaRed);
+            sepiaGreen = cap(sepiaGreen);
+            sepiaBlue = cap(sepiaBlue);
+        }
+    }
 }
 
 // Reflect image horizontally
